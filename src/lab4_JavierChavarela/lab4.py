392a969a22893dec5bd053b82c8987c9aacf1e3a
JavierChavarela <chavarj@csu.fullerton.edu>
2016-11-20 21:04:44 -0800

    added my lab4
    Added my lab 4 which I need to update

---
+++ src/lab4_JavierChavarela/lab4.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 20, 2016
+Intro to Python Programming - CSUF
+Lab 4: Standard Modules, Packages, and Classes
+BankCustomer Driver
+lab4.py
+@author: Javier Chavarela
+'''
+
+
+
+bankCustomer.withdraw(100.00)
+bankCustomer.deposit(10000.00)
+bankCustomer.withdraw(10000000.00)
